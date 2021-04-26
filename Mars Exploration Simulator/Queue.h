ff784c1d8bbe4e4e4376e3df5801a0c168a31ee7
Nabil <mohamed.nabi.5596@gmail.com>
2021-04-26 12:45:17 +0200

    stack implementation

---
+++ Mars Exploration Simulator/Queue.h
@@ -0,0 +1,100 @@
+#pragma once
+#include"DataStructures/Node.h"
+
+
+template<class dataType>
+class Queue
+{
+
+private:
+	Node<dataType>* top;
+	Node<dataType>* rear;
+
+public:
+	
+	Queue();
+	
+	Queue(const Queue<dataType>&);
+
+	bool enqueue(const dataType&);
+	
+	bool dequeue(dataType&);
+    
+	bool peek(dataType&);
+	
+	bool isEmpty();
+};
+//default constructor
+template<class datatype>
+Queue<datatype>::Queue() :top(nullptr),rear(nullptr){}
+
+
+
+//copy constructor
+template<class dataType>
+Queue<dataType>::Queue(const Queue& q2)
+{
+	while (!this->isEmpty())  // make sure the destination is empty
+	{
+		this->dequeue();
+	}
+	Node<dataType>* front = q2.top;
+	while (front)
+	{
+		this->enqueue(front->getItem());
+		front = front->getNext();
+	}
+}
+
+
+
+// function enqueue
+template<class dataType>
+bool Queue<dataType>::enqueue(const dataType& item)
+{
+	Node<dataType>* temp(item,nullptr);
+	if (temp) {
+		if (!top)   //if en queue first time
+			top = temp;
+		rear->setNext(temp);
+		rear = temp;
+		return true;
+	}
+	return false; //if not available memory
+}
+
+
+
+
+//function dequeue
+template<class dataType>
+bool Queue<dataType>::dequeue(dataType& item)
+{
+	if (isEmpty())return false;
+	Node<dataType>* temp = top;
+	top = top->getNext();
+	item = temp->getItem();
+	delete temp;
+	if (rear == temp)rear = nullptr; //if dequeue the last item
+	return true;
+}
+
+
+
+//function peek
+template<class dataType>
+bool Queue<dataType>::peek(dataType& item)
+{
+	if (isEmpty)return false;
+	item = top->getItem();
+	return  true;
+}
+
+
+
+//function isEmpty
+template<class dataType>
+bool Queue<dataType>::isEmpty()
+{
+	return (top == nullptr);
+}
