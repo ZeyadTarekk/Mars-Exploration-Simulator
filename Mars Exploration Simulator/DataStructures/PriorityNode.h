73ecfa7d3b1465b1ccb93e820cf20f82b47c9064
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2021-04-26 03:46:48 +0200

    PriorityNode Edited make priority const and tested

--- Mars Exploration Simulator/DataStructures/PriorityNode.h
+++ Mars Exploration Simulator/DataStructures/PriorityNode.h
@@ -9,32 +9,32 @@ class PriorityNode:public Node<type>
 public:
 
 	//Construtors
-	PriorityNode(long long);									//Recives priority only
-	PriorityNode(const type&, long long);						//Recives item and priority 
-	PriorityNode(const type&, PriorityNode<type>*,long long);	//Recives item ,next and priority
+	PriorityNode(const long long&);									//Recives priority only
+	PriorityNode(const type&, const long long&);						//Recives item and priority 
+	PriorityNode(const type&, PriorityNode<type>*,const long long&);	//Recives item ,next and priority
 	
 	//Additional Setters and getters
-	void setPriority(const long long);
+	void setPriority(const long long&);
 	long long getPriority() const;
 };
 
 template<class type>
-inline PriorityNode<type>::PriorityNode(long long recivedPriority):priority(recivedPriority)
+inline PriorityNode<type>::PriorityNode(const long long& recivedPriority):priority(recivedPriority)
 {
 }
 
 template<class type>
-inline PriorityNode<type>::PriorityNode(const type& recivedItem, long long recivedPriority):Node<type>(recivedItem),priority(recivedPriority)
+inline PriorityNode<type>::PriorityNode(const type& recivedItem,const long long& recivedPriority):Node<type>(recivedItem),priority(recivedPriority)
 {
 }
 
 template<class type>
-inline PriorityNode<type>::PriorityNode(const type&recivedItem, PriorityNode<type>*recivedNext, long long recivedPriority):Node<type>(recivedItem, recivedNext),priority(recivedPriority)
+inline PriorityNode<type>::PriorityNode(const type&recivedItem, PriorityNode<type>*recivedNext,const long long& recivedPriority):Node<type>(recivedItem, recivedNext),priority(recivedPriority)
 {
 }
 
 template<class type>
-inline void PriorityNode<type>::setPriority(const long long recivedPriority)
+inline void PriorityNode<type>::setPriority(const long long& recivedPriority)
 {
 	priority = recivedPriority;
 }
