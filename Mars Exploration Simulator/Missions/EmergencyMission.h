ea15c0ee61eb1aaabf775dbbbb36dd3cc3f52c1f
Nabil <mohamed.nabi.5596@gmail.com>
2021-05-02 01:33:18 +0200

    abstract class mission && class emergency

--- Mars Exploration Simulator/Missions/EmergencyMission.h
+++ Mars Exploration Simulator/Missions/EmergencyMission.h
@@ -1,5 +1,32 @@
 #pragma once
-class EmergencyMission
+#include"Mission.h"
+class EmergencyMission :public Mission
 {
+private:
+	const double priority;
+	double calculatePriority(); //utility function will be called in only constructor
+public:
+	EmergencyMission(int, int, int, int, double);
+	double getPriority();
 };
 
+
+
+EmergencyMission::EmergencyMission(int fD, int tL, int mD, int i, double s) :
+	Mission(fD, tL, mD, i, s),
+	priority(calculatePriority())
+{}
+
+
+
+double EmergencyMission::calculatePriority()
+{
+	return getSignificance() / (getFormulationDay() * getMissionDuration() * getTargetLocation());
+	//this equation is primary one 
+}
+
+
+double EmergencyMission::getPriority() 
+{
+	return priority;
+}
--- Mars Exploration Simulator/Missions/Mission.h
+++ Mars Exploration Simulator/Missions/Mission.h
@@ -1,5 +1,48 @@
 #pragma once
+#include"../Rovers/Rover.h"
 class Mission
 {
+private:
+	const int formulationDay,
+		targetLocation,
+		missionDuration,
+		id;
+	const double significance;
+	Rover * assignedRover;   //the attribute isn't const because it's assigned after formulation
+public:
+	Mission(int, int, int, int, double);
+	virtual ~Mission() = 0;  //make destructor pure virtual to make the class abstract
+	void assignRover(Rover*);
+	int getFormulationDay() const;
+	int getTargetLocation() const;
+	int getMissionDuration() const;
+	double getSignificance() const;
+	Rover * getAssignedRover() const;
 };
 
+
+Mission::Mission(int fD, int tL, int mD, int i, double s) :
+	formulationDay(fD),
+	targetLocation(tL),
+	missionDuration(mD),
+	id(i),
+	significance(s)
+{}
+
+
+
+void Mission::assignRover(Rover* rV)
+{
+	assignedRover = rV;
+}
+
+
+int Mission::getFormulationDay()const { return formulationDay; }
+
+int Mission::getTargetLocation()const { return targetLocation; }
+
+int Mission::getMissionDuration()const { return missionDuration; }
+
+double Mission::getSignificance() const { return significance; }
+
+Rover* Mission::getAssignedRover() const { return assignedRover; }
