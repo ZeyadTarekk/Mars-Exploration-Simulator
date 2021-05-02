f8bea9c25a33774792491a824d55fef129c58948
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2021-05-02 04:32:46 +0200

    Rover Base class and its test Hamza

--- Mars Exploration Simulator/Rovers/Rover.cpp
+++ Mars Exploration Simulator/Rovers/Rover.cpp
@@ -1,1 +1,103 @@
 #include "Rover.h"
+
+Rover::Rover(int roverspeed, int checkupCount, int Days)
+{
+	ID = idCounter;
+	speed = (roverspeed > 0 ? roverspeed : 0);
+	checkupDays = (Days>0?Days:0);
+	checkupMissions = (checkupCount > 0 ? checkupCount : 0);
+
+	missionId = -1;
+	missionDuration = -1;
+	missionOrCheckupEndDay = -1;
+	missionLocation = -1;
+
+	missionsNumber = 0;
+	
+	
+	needCheckup = false;
+
+	idCounter++;
+
+}
+
+void Rover::reset()
+{
+	missionId = -1;
+	missionDuration = -1;
+	missionOrCheckupEndDay = -1;
+	missionLocation = -1;
+}
+
+void Rover::assignMission(int idMission, int duration,int location)
+{
+	missionId = (idMission>0?idMission:0);
+	missionDuration = (duration>0?duration:0);
+	missionLocation = (location > 0? location : 0);
+	missionOrCheckupEndDay = missionDuration + ceil( ((float)missionLocation / speed ) /25.0);
+	
+	missionsNumber++;
+	if (missionsNumber%checkupMissions==0)
+		needCheckup = true;
+}
+
+void Rover::assignCheckup()
+{
+	missionOrCheckupEndDay = checkupDays;
+	//missionsNumber = 0;
+	needCheckup = false;
+	reset();
+}
+
+unsigned int Rover::getID()
+{
+	return ID;
+}
+
+int Rover::getSpeed()
+{
+	return speed;
+}
+
+int Rover::getCheckupReqDays()
+{
+	return checkupDays;
+}
+
+int Rover::getCheckupReqMissions()
+{
+	return checkupMissions;
+}
+
+int Rover::getMissionID()
+{
+	return missionId;
+}
+
+int Rover::getMissionDuration()
+{
+	return missionDuration;
+}
+
+int Rover::getMissionLocation()
+{
+	return missionLocation;
+}
+
+int Rover::getMissionOrCheckupEndDay()
+{
+	return missionOrCheckupEndDay;
+}
+
+int Rover::getMissionNumber()
+{
+	return missionsNumber;
+}
+
+bool Rover::getneedCheckup()
+{
+	return needCheckup;
+}
+
+
+unsigned int Rover::idCounter = 1;
--- Mars Exploration Simulator/Rovers/Rover.h
+++ Mars Exploration Simulator/Rovers/Rover.h
@@ -1,5 +1,47 @@
 #pragma once
+#include<iostream>
+
 class Rover
 {
+	static unsigned int idCounter;
+	
+	//essential info ---> given
+	unsigned int ID;
+	int speed;
+	int checkupDays;
+	int checkupMissions;	
+
+	//missions info
+	int missionId;				//given
+	int missionDuration;		//given
+	int missionLocation;		//given
+	int missionOrCheckupEndDay;	//calculated
+	
+	//checkupInfo
+	int missionsNumber;			//calculated
+	bool needCheckup;			//calculated
+
+public:
+	Rover(int roverspeed, int checkupCount, int Days); //essential data only
+
+	void reset();
+
+	//setters
+	void assignMission(int idMission,int duration, int location);
+	void assignCheckup();
+
+	//getters
+	unsigned int getID();
+	int getSpeed();
+	int getCheckupReqDays();
+	int getCheckupReqMissions();
+
+	int getMissionID();
+	int getMissionDuration();
+	int getMissionLocation();
+	int getMissionOrCheckupEndDay();
+
+	int getMissionNumber();
+	bool getneedCheckup();
 };
 
