59e8f6aec8513ec42cd325210d24ae0fe5de7a67
Ziad Sherif Muhammed Ibrahim <78238570+ziadsherif00@users.noreply.github.com>
2021-05-08 06:02:25 +0200

    List

---
+++ Mars Exploration Simulator/DataStructures/List.h
@@ -0,0 +1,26 @@
+#pragma once
+#include"Node.h"
+template<class itemType>
+class List
+{
+public:
+	List(){}
+	List(const List<itemType> &L2)
+	{
+	}
+	bool contains()const
+	{	
+	}
+	bool search(itemType&item)const
+	{}
+	bool insertEnd(itemType& item)
+	{}
+	bool deleteNode(itemType&item)
+	{}
+	void deleteAll()
+	{}
+	~List()
+	{}
+
+};
+
