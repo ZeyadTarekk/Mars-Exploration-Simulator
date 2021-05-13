ee42adcf72600efcb2a33bcb1d85093e833196e9
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2021-05-14 00:49:50 +0200

    UI ,read ,Print

---
+++ Mars Exploration Simulator/UI/Source1.cpp
@@ -0,0 +1,19 @@
+//#include<iostream>
+//#include<fstream>
+//using namespace std;
+//int main()
+//{
+//	ofstream out;
+//	out.open("OutputFiles/Output.txt");
+//	if (out.is_open())
+//		out << "Hello" << endl;
+//	else
+//		cout << "Error";
+//	ifstream in("InputFiles/Input.txt");
+//	int x;
+//	in >> x;
+//	cout << "X = " << x<<" ";
+//	in >> x;
+//	cout << "X = " << x << " ";
+//	return 0;
+//}
