e7cba4c2f9c72335dd5d177b481073c96874c80d
ZeyadTa <Zeyad.ta01@gmail.com>
2021-05-13 03:13:03 +0200

    By Tarek

---
+++ Mars Exploration Simulator/Events/CancelationEvent.cpp
@@ -0,0 +1,18 @@
+#include "CancelationEvent.h"
+
+CancelationEvent::CancelationEvent(int day, int i): Event(day)
+{
+	id = i;
+}
+
+void CancelationEvent::execute(MarsStation* mStation)
+{
+	MountainousMission mountMission(0, 0, 0, id, 0);
+	int indexOfMission = mStation->IndexOfMountainousMission(mountMission);
+	if (indexOfMission != -1)
+		mStation->removeMountainousMission(indexOfMission);
+}
+
+CancelationEvent::~CancelationEvent()
+{
+}
--- Mars Exploration Simulator/MarsStation/Source.cpp
+++ Mars Exploration Simulator/MarsStation/Source.cpp
@@ -2,6 +2,7 @@
 #include"../MarsStation/MarsStation.h"
 #include"../Events/PromotionEvent.h"
 #include"../Events/FormulationEvent.h"
+#include"../Events/CancelationEvent.h"
 using namespace std;
 int main()
 {
@@ -9,7 +10,8 @@ int main()
 	FormulationEvent f1(1, 1, 'M', 1, 1, 1);
 	FormulationEvent f2(2, 2, 'M', 2, 2, 2);
 	f1.execute(m);f2.execute(m);
-	PromotionEvent Pevent(1,-1);
+	PromotionEvent Pevent(1,2);
+
 	cout << "\nMountainous before promotion" << endl;
 	m->PrintMountList();
 	Pevent.execute(m);
@@ -17,5 +19,25 @@ int main()
 	m->PrintMountList();
 	cout << "\nEmergency After promotion" << endl;
 	m->PrintEmergencyList();
+
+	cout << "\n++++++++++++++++++++++++++++++++++++++++++++++++++++++\n";
+	cout << "Testing Cancelation\n";
+
+	FormulationEvent f3(6, 3, 'M', 1, 1, 1);
+	FormulationEvent f4(7, 4, 'M', 2, 2, 2);
+	f3.execute(m);
+	f4.execute(m);
+	CancelationEvent cEvent(3, 3);
+	CancelationEvent cEvent2(3, 3);
+	cout << "\nMountainous before promotion" << endl;
+	m->PrintMountList();
+	cEvent.execute(m);
+	cEvent2.execute(m);
+	cout << "\nMountainous After cancelation" << endl;
+	m->PrintMountList();
+	CancelationEvent cEvent3(3, 4);
+	cEvent3.execute(m);
+	m->PrintMountList();
 	return 0;
+
 }
